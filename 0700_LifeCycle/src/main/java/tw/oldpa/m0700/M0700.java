e4fc9e0003b9c0b50374b2ad34d0a12c131ad9b4
oldpa88 <vpoint88@gmail.com>
2021-12-02 14:00:00 +0800

    20211201 update

--- 0700_LifeCycle/src/main/java/tw/oldpa/m0700/M0700.java
+++ 0700_LifeCycle/src/main/java/tw/oldpa/m0700/M0700.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class M0700 extends AppCompatActivity {
     private final String TAG = "oldpa=>";
@@ -50,4 +53,33 @@ public class M0700 extends AppCompatActivity {
         Log.d(TAG, "onDestroy()");
     }
 
+    //*******************************************************************************************
+    @Override
+    public void onBackPressed() {
+//super.onBackPressed();//不執行這行
+        Toast.makeText(getApplication(), "禁用返回鍵", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.m0700, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                this.finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //*******************************************************************************************
 }
